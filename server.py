5c66028bcf7c42f7e2945c52edfcc462918b12f0
ahmed.gater <ahmed.gater@gmail.com>
2025-11-16 12:22:35 +0100

    adding chroma feature

--- server.py
+++ server.py
@@ -1,4 +1,12 @@
 from fastmcp import FastMCP
+import chromadb
+from chromadb.api.models.Collection import Collection
+from langchain_community.vectorstores import Chroma
+
+CHROMA_API_KEY='ck-EjaCYQBCwJRbW4Wh1eGJRzDfZAiqAw73BbYUbFYAS5Ux'
+CHROMA_TENANT='1cedb133-10a5-4fa3-bbfa-ff913e057895'
+CHROME_DATABASE='TEST_RAG'
+CHROMA_COLLECTION = "uipath_sec_doc"
 
 mcp = FastMCP(name="My First MCP Server")
 
@@ -16,6 +24,37 @@ def get_config() -> dict:
 def personalized_greeting(name: str) -> str:
     """Generates a personalized greeting for the given name."""
     return f"Hello, {name}! Welcome to the MCP server."
+
+def createChromaClient():
+    return  chromadb.CloudClient(
+                    api_key=CHROMA_API_KEY,
+                    tenant=CHROMA_TENANT,
+                    database=CHROME_DATABASE
+                )
+
+@mcp.tool()
+def retrieve_uipath_security_doc(query:str) ->list[str]:
+    """
+     Used to search the UiPath security documents related to GenAI
+        Returns:
+            List of found documents
+        """
+    client = createChromaClient()
+    vectorstore = Chroma(
+        client=client,
+        collection_name=CHROMA_COLLECTION
+    )
+
+    retriever = vectorstore.as_retriever(
+        search_type="similarity",
+        search_kwargs={"k": 20}
+    )
+    docs = retriever.invoke(query)
+    r=[]
+    for doc in docs:
+       r.append(doc.page_content)
+    return r
+
 
 if __name__ == "__main__":
     mcp = FastMCP(name="My First MCP Server")
